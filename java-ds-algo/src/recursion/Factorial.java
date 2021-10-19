838e7063cce26271dc857c9dc0a5314a18584e3c
Akash Kumar Pal <akashpal@drreddys.com>
2021-10-20 02:05:23 +0530

    factrial recursion

--- java-ds-algo/src/recursion/Factorial.java
+++ java-ds-algo/src/recursion/Factorial.java
@@ -1,7 +1,27 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class Factorial {
 	public static void main(String temp[]) {
-		System.out.println("Hello world of Java");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a positive number: ");
+		Integer input = sc.nextInt();
+		if (input < 0) {
+			System.out.println("Please enter a positive number.");
+			main(null);
+		} else {
+			Integer result = factorialCalculation(input);
+			System.out.println(result);
+		}
+		sc.close();
+	}
+
+	private static Integer factorialCalculation(Integer input) {
+		if (input == 0) {
+			return 1;
+		} else {
+			return input * factorialCalculation(input - 1);
+		}
 	}
 }
